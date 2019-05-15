14f4100ebb1fee55e1216066efc0be2433f49944
aidan <ajm67@njit.edu>
2019-05-15 16:50:26 -0700

    Create a database seed to generate 50 cars. - 50 card generated

--- database/seeds/CarTableSeeder.php
+++ database/seeds/CarTableSeeder.php
@@ -13,4 +13,16 @@ class CarTableSeeder extends Seeder
     {
         factory('App/Carfactory.php', 10)->create();
     }
+};
+
+public function run()
+{
+    factory(App\User::class, 50)->create()->each(function (Faker\generator $faker) {
+        return array(
+            'Make' => $faker->sentence(5),
+            'Model' => $faker->realText(rand(80, 600)),
+            'Year'  => $faker->date(),
+
+        $user->posts()->save(factory(App\Post::class)->make()));
+    });
 }
